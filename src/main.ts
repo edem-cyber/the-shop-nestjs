123820d7170384bdbc0f5a4c3f163e252c6ac2c9
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-18 01:07:03 +0000

    Add Sentry integration for performance monitoring

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
+import { ProfilingIntegration } from '@sentry/profiling-node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  Sentry.init({
+    dsn: 'https://a8a99ac2b8ebc4f93a43fffa3696a0b7@o4505333410496512.ingest.sentry.io/4506760845852672',
+    integrations: [new ProfilingIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
 
   const config = new DocumentBuilder()
     .setTitle('My API')
