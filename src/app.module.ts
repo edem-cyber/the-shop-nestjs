5033681c30db46eccd4a74e9a8801569134f6e5e
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-18 01:06:58 +0000

    Add JwtModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule],
+  imports: [UsersModule, PrismaModule, AuthModule, JwtModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
