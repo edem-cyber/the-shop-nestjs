623b0e174651c999492339c3fad33a84e7558e1f
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-18 01:07:28 +0000

    Add new DTOs for sign-in, email request, OTP, and password reset

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,1 +1,34 @@
-export class CreateAuthDto {}
+import { IsEmail, IsString, IsNumber, MinLength } from 'class-validator';
+
+export class SignInDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  password: string;
+}
+
+export class RequestEmailDto {
+  @IsEmail()
+  email: string;
+}
+
+export class OtpDto {
+  @IsNumber()
+  otp: number;
+}
+
+export class ResetPasswordDto {
+  @IsEmail()
+  email: string;
+
+  @IsNumber()
+  @MinLength(3)
+  otp: number;
+
+  @IsString()
+  @MinLength(8)
+  newPassword: string;
+}
+// Compare this snippet from src/auth/auth.service.ts:
--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,15 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateAuthDto } from './create-auth.dto';
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  SignInDto,
+  OtpDto,
+  RequestEmailDto,
+  ResetPasswordDto,
+} from './create-auth.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(SignInDto) {}
+
+export class UpdateOtpDto extends PartialType(OtpDto) {}
+
+export class UpdateRequestEmailDto extends PartialType(RequestEmailDto) {}
+
+export class UpdateResetPasswordDto extends PartialType(ResetPasswordDto) {}
