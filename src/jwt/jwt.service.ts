fdf1bcd95876ad451880f98cd6f77dfcde818077
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-19 21:24:37 +0000

    Refactor JwtService to use NestJwtService

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@nestjs/common';
-// import { CreateJwtDto } from './dto/create-jwt.dto';
-// import { UpdateJwtDto } from './dto/update-jwt.dto';
+import { JwtService as NestJwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class JwtService {
-  // create(createJwtDto: CreateJwtDto) {
-  //   return 'This action adds a new jwt';
-  // }
-  // findAll() {
-  //   return `This action returns all jwt`;
-  // }
-  // findOne(id: number) {
-  //   return `This action returns a #${id} jwt`;
-  // }
-  // update(id: number, updateJwtDto: UpdateJwtDto) {
-  //   return `This action updates a #${id} jwt`;
-  // }
-  // remove(id: number) {
-  //   return `This action removes a #${id} jwt`;
-  // }
+  constructor(private readonly nestJwtService: NestJwtService) {}
+
+  async sign(payload: any): Promise<string> {
+    return this.nestJwtService.sign(payload);
+  }
+
+  async verify(token: string): Promise<any> {
+    return this.nestJwtService.verify(token);
+  }
 }
