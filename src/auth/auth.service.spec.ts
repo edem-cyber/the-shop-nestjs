ae92908ad8999228c2b08b3b5cc0b9f63101c4cf
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-19 21:24:07 +0000

    Add PrismaService and JwtService to AuthService providers and import JwtModule

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { JwtModule } from '../jwt/jwt.module';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService, PrismaService, JwtService],
+      imports: [JwtModule], // Import the JwtModule here
     }).compile();
 
     service = module.get<AuthService>(AuthService);
