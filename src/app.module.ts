79b15c0f136c200190b2f0c2c40deb7ea2db2ed7
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2024-02-19 21:23:50 +0000

    Add UsersService to providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from './jwt/jwt.module';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule, JwtModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UsersService],
 })
 export class AppModule {}
